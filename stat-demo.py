ad182ce6cc8c088e80eac6dd7a2659ab976afc88
Yunfeng Wang <vra@users.noreply.github.com>
2015-04-25 23:41:36 +0800

    修改了时间计算方法，将回复间隔大于10分钟的回复舍弃掉；去掉了手动添加聊天昵称的代码

--- stat-demo.py
+++ stat-demo.py
@@ -2,17 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import re
-#将这里的name1,name2修改为聊天记录里面的两个人的用户名
-name1 = u"name1"
-name2 = u"name2"
+#将path/to/chat-recor改为实际路径
+f = open("/path/to/chat-record", "r")
+record = open("/path/to/chat-record", "r").read().decode('utf-8')
 
-#将这里的/path/to/chat-record改为到聊天记录的路径
-record = open("/path/to/chat-record.txt", "r").read().decode('utf-8')
+#get 2 nicknames
+l = (f.readline()).split(' ')
+name1 = l[3]
+
+f.readline()
+f.readline()
+
+l = (f.readline()).split(' ')
+name2 = l[3]
+
+#转换为unicode
+name1 = unicode(name1, 'utf-8')
+name2 = unicode(name2, 'utf-8')
+
+print name1,name2
 day = re.findall('(\d{4}-\d{2}-\d{2})',record)
 times = re.findall('\d{4}-\d{2}-\d{2} (\d{2}):(\d{2}):(\d{2})  (%s|%s)' %(name1, name2), record)
 
 if (len(times) < 1):
-	print '聊天记录为空'
+	print '没有聊天内容'
 	exit()
 
 avgtime1=[]
@@ -21,7 +34,7 @@ avgtime2=[]
 def countTime(h1,m1,s1,h2,m2,s2):
 	return (h2 - h1) * 3600 + (m2 - m1) * 60 + s2 - s1
 
-print '聊天日期：',day[0]
+print '聊天日期：',day[0],'到',day[len(day) - 1]
 print '共', len(times),'条聊天记录'
 
 if(times[0][3] == name1):
@@ -51,12 +64,19 @@ for time in times[2:]:
 	second3= time[2]
 
 	if (time[3] == name1):
-		avgtime1.append(countTime(int(hour1),int(min1),int(second1),int(hour3),int(min3),int(second3)))		
+		timeGap1 =countTime(int(hour1),int(min1),int(second1),int(hour3),int(min3),int(second3))
+		if (timeGap1 > 60 * 5):
+			continue
+
+		avgtime1.append(timeGap1)		
 		hour1=hour3
 		min1=min3
 		second1=second3
 	if (time[3] == name2):
-		avgtime2.append(countTime(int(hour2),int(min2),int(second2),int(hour3),int(min3),int(second3)))		
+		timeGap2 =countTime(int(hour2),int(min2),int(second2),int(hour3),int(min3),int(second3))
+		if (timeGap2 > 60 * 5):
+			continue
+		avgtime2.append(timeGap2)		
 		hour2=hour3
 		min2=min3
 		second2=second3
